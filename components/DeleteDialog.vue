a4d91b63f24c1d4a2756e6972293e340bd1eb99c
gmarcha <gmarcha@github.com>
2022-05-12 10:16:54 -0400

    update components: add delete dialog

---
+++ components/DeleteDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-card-title class="text-center">
+      <v-spacer />Supprimer {{ title }} ?<v-spacer />
+    </v-card-title>
+    <v-card-text class="text-center">
+      <slot></slot>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="red" dark :elevation="0" class="mb-2" @click="action">
+        confirmer
+      </v-btn>
+      <v-spacer />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String as () => string,
+      required: true,
+      default: '',
+    },
+    action: {
+      type: Function,
+      required: true,
+      default: () => {},
+    },
+  },
+})
+</script>
